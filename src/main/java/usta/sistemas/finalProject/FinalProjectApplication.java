e3872a0a07f0fe90e5e067fe580cbb4c5ec6ccaf
santiagohernandez <santiago.hernandez@usantoto.edu.co>
2022-11-18 13:15:49 -0500

    first commit

---
+++ src/main/java/usta/sistemas/finalProject/FinalProjectApplication.java
@@ -0,0 +1,21 @@
+package usta.sistemas.finalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
